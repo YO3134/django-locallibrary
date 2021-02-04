c310b71990594fd32ae70ae2cd65ca5dd4326401
YO3134 <nishiura.145@gmail.com>
2021-02-04 23:55:48 +0900

    Add class Genre to catalog/models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='本ジャンルを入力してください(SF,古典)')
+
+    def __str__(self):
+        return self.name
