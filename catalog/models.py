754cf222e1fa552c559a92a8948f445d93dd1aae
YO3134 <nishiura.145@gmail.com>
2021-02-05 23:10:23 +0900

    Add class Book to catalog/models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,18 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='ほんの簡単な説明を入力してください')
+    isbn = models.CharField(
+        "ISBN", max_length=13, help_text='13桁 <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='この本のジャンルを選択してください')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args={str(self.id)})
